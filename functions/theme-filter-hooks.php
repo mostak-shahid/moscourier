91754b42861b0c5cb3bd90068d809329a6bde848
Md. Mostak Shahid <mostak.shahid@gmail.com>
2020-03-20 18:09:34 +0600

    Update theme-filter-hooks.php

--- functions/theme-filter-hooks.php
+++ functions/theme-filter-hooks.php
@@ -37,3 +37,19 @@ add_filter('wpcf7_form_elements', function($content) {
     $content = preg_replace('/<(span).*?class="\s*(?:.*\s)?wpcf7-form-control-wrap(?:\s[^"]+)?\s*"[^\>]*>(.*)<\/\1>/i', '\2', $content);
     return $content;
 });
+function make_tml_forms_bootstrap_compatible() {
+    foreach ( tml_get_forms() as $form ) {
+        foreach ( tml_get_form_fields( $form ) as $field ) {
+            if ( 'hidden' == $field->get_type() ) {
+                continue;
+            }
+
+            $field->render_args['before'] = '<div class="form-group">';
+            $field->render_args['after'] = '</div>';
+            if ( 'checkbox' != $field->get_type() ) {
+                $field->add_attribute( 'class', 'form-control' );
+            }
+        }
+    }
+}
+add_action( 'init', 'make_tml_forms_bootstrap_compatible' );
